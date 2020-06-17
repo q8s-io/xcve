0d3a40cceae989dae6c721872cd9428fb14f4e42
zhaoxueqian <zhaoxueqian@360.cn>
2020-06-17 17:26:33 +0800

    scann cve

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -* - coding: UTF-8 -* -
+
 from fastapi import FastAPI, HTTPException
 from py2neo import Graph
 from py2neo.data import Node, Relationship
@@ -8,13 +11,11 @@ graph = Graph(host="xcve-neo4j", password='streams')
 
 
 @app.get("/")
-def read_root():
-    ret = graph.run('match (n) return n limit 5').data()
-    return {"Hello": json.dumps(ret)}
-
 @app.get("/random")
 def random():
-    return {"random": '{}'}
+    # generate something random that maybe useful on main page.
+    ret = graph.run('match (cve:CVE) with cve, rand() as number return cve order by number limit 10').data()
+    return ret
  
 @app.get("/search/{keyword}")
 def search(keyword: str=None):
@@ -22,19 +23,33 @@ def search(keyword: str=None):
 
 @app.get("/cve/{cve_id}")
 def scann_cve(cve_id: str):
+    # abs match cve-number
     if not cve_id:
         raise HTTPException(status_code=404, detail="No cve_id specified!")
-    ret = graph.run('match (n:CVE) where n.name="{}" return  n'.format(cve_id)).data()
-    return {cve_id: json.dumps(ret)}
+    cve = graph.run('match (cve:CVE) where cve.name="{}" return cve'.format(cve_id)).data()[0]
+    relations = graph.run('match (cve:CVE)-[r]->(x) where cve.name="{}" return r,x'.format(cve_id)).data()
+    
+    for i in relations:
+        rlname = i.get('r').__class__.__name__
+        if not rlname in cve:
+            cve[rlname] = []
+        x = dict(i.get('x'))
+        x['label'] = str(i.get('x').labels)
+        cve[rlname].append(x)
+
+    return {cve_id: cve}
 
 @app.get("/vendor/{vendor_name}")
 def search_vendor(vendor_name: str):
+    # abs search by vendor
     return {"vendor_name": vendor_name}
 
 @app.get("/product/{product_name}")
 def search_product(product_name: str):
+    # abs search by product
     return {"product_name": product_name}
 
 @app.get("/proversion/{proversion_name}")
 def proversion(proversion_name: str):
+    # match procut-version
     return {"proversion_name": proversion_name}
