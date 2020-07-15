3e410537aec6f51a436ba6032f7730c24d251e73
dingrenyi <dingrenyi@360.cn>
2020-07-15 19:16:59 +0800

    修改host

--- fe/src/api/search.js
+++ fe/src/api/search.js
@@ -5,19 +5,19 @@ import { get } from "@/helper/http/index.js";
  * 获取
  */
 export const getSuggest = params => {
-  return get(`http://docker1001.idp.shyc2.qihoo.net:8099/sug`, params);
+  return get(`/sug`, params);
 };
 
 /*
  * 获取
  */
 export const getDetail = params => {
-  return get(`http://docker1001.idp.shyc2.qihoo.net:8099/cve`, params);
+  return get(`/cve`, params);
 };
 
 /*
  * 获取
  */
 export const getConf = params => {
-  return get(`http://docker1001.idp.shyc2.qihoo.net:8099/frontconf`, params);
+  return get(`/frontconf`, params);
 };
